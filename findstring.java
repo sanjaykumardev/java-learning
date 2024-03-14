6d60d55ecc1439728a345f2b93476c609eecea00
sanjaykumardev <duderaj1976@gmail.com>
2024-03-14 08:58:30 +0530

    change

--- findstring.java
+++ findstring.java
@@ -1,27 +1,27 @@
-import java.util.*;
+// import java.util.*;
 
 
 
 
-class findstring {
-  public static void main(String[] args) {
+// class findstring {
+//   public static void main(String[] args) {
     
-    Scanner sc =  new Scanner(System.in);
-    System.out.println(s);
-    String s = sc.nextLine();
+//     Scanner sc =  new Scanner(System.in);
+//     System.out.println(s);
+//     String s = sc.nextLine();
     
-    //? function call
+//     //? function call
      
 
        
 
-  }
+//   }
 
 
 
-  public static void string(String name) {
+//   public static void string(String name) {
     
-    int i;
+//     int i;
    
-  }
-}
+//   }
+// }
